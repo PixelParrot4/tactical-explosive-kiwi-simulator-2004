786e3f39a5928a50067181217689e2baae58edca
PixelParrot4 <bugejagreg@gmail.com>
2025-05-04 19:44:05 -0700

    level selecting button works now
    animations work and also sfx
    
    bugfix: end screen buttons no longer have outlines when pressed

---
+++ level_selecting_button.gd
@@ -0,0 +1,26 @@
+extends Button
+@onready var button = $button
+@onready var number_display = $number_display
+@export var level:int#level you are taken to once button pressed
+
+#display frame whose number corresponds with var level
+func _ready() -> void:
+	number_display.frame=level
+
+#animation code
+func _on_button_down() -> void:
+	button.play("pressed")
+	number_display.position=Vector2(38,38)
+	$"../UISelect".play()#sfx code (1/2)
+func _on_button_up() -> void:
+	button.play("unpressed")
+	number_display.position=Vector2(38,30)
+
+#sfx code (2/2)
+func _on_mouse_entered() -> void:
+	$"../UIHover".play()
+
+#level switching code borrowed from level.gd
+func _on_pressed() -> void:
+	var level_path = "res://level_" + str(level) + ".tscn"
+	get_tree().change_scene_to_file(level_path)
