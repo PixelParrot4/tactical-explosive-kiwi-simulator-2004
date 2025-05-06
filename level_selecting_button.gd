fc1b905a43aad171b4ef26a27d08d507bab40dc9
PixelParrot4 <bugejagreg@gmail.com>
2025-05-06 10:44:08 -0700

    moved button to vbox
    fixed referencing, considering using "@export var" though
    
    BUG: button collision does not align with texture

--- level_selecting_button.gd
+++ level_selecting_button.gd
@@ -1,6 +1,7 @@
 extends Button
 @onready var button = $button
 @onready var number_display = $number_display
+@onready var UIHover:AudioStreamPlayer=$"/root/MainMenu/UIHover"
 @export var level:int#level you are taken to once button pressed
 
 #display frame whose number corresponds with var level
@@ -11,15 +12,14 @@ func _ready() -> void:
 func _on_button_down() -> void:
 	button.play("pressed")
 	number_display.position=Vector2(38,38)
-	$"../UISelect".play()#sfx code (1/2)
+	$"/root/MainMenu/UISelect".play()#sfx code (1/2)
 func _on_button_up() -> void:
 	button.play("unpressed")
 	number_display.position=Vector2(38,30)
 
 #sfx code (2/2)
 func _on_mouse_entered() -> void:
-	$"../UIHover".play()
-
+	UIHover.play()
 #level switching code borrowed from level.gd
 func _on_pressed() -> void:
 	var level_path = "res://level_" + str(level) + ".tscn"
